55b0d9781983db996f0450303ad1ab4f5d47e8cf
thk1 <thk@mail.de>
2017-09-07 13:34:46 +0200

    Add documentation

--- src/lib.rs
+++ src/lib.rs
@@ -6,6 +6,72 @@
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
+//! This library implements a wrapper type called `SendWrapper` which allows you to move around non-[`Send`]-types
+//! between threads, as long as you access the contained value only from within the original thread. You also have to
+//! make sure that the wrapper is dropped from within the original thread. If any of these constraints is violated,
+//! a panic occurs.
+//!
+//! The idea for this library was born in the context of a [`GTK+`]/[`gtk-rs`]-based application. [`GTK+`] applications
+//! are strictly single-threaded. It is not allowed to call any [`GTK+`] method from a thread different to the main
+//! thread. Consequently, all [`gtk-rs`] structs are non-[`Send`].
+//!
+//! Sometimes you still want to do some work in background. It is possible to enqueue [`GTK+`] calls from there to be
+//! executed in the main thread [using `Glib`]. This way you can know, that the [`gtk-rs`] structs involved are only
+//! accessed in the main thread and will also be dropped there. This library makes it possible that [`gtk-rs`] structs
+//! can leave the main thread at all, like required in the given
+//!
+//! # Examples
+//!
+//! ```rust
+//! use send_wrapper::SendWrapper;
+//! use std::rc::Rc;
+//! use std::thread;
+//! use std::sync::mpsc::channel;
+//!
+//! // This import is important. It allows you to unwrap the value using deref(),
+//! // deref_mut() or Deref coercion.
+//! use std::ops::{Deref, DerefMut};
+//!
+//! // Rc is a non-Send type.
+//! let value = Rc::new(42);
+//!
+//! // We now wrap the value with `SendWrapper` (value is moved inside).
+//! let wrapped_value = SendWrapper::new(value);
+//!
+//! // A channel allows us to move the wrapped value between threads.
+//! let (sender, receiver) = channel();
+//!
+//! let t = thread::spawn(move || {
+//!
+//!		// This would panic (because of dereferencing in wrong thread):
+//!		// let value = wrapped_value.deref();
+//!
+//!	 // Move SendWrapper back to main thread, so it can be dropped from there.
+//!		// If you leave this out the thread will panic because of dropping from wrong thread.
+//!	 sender.send(wrapped_value).unwrap();
+//!
+//! });
+//!
+//! let wrapped_value = receiver.recv().unwrap();
+//!
+//! // Now you can use the value again.
+//! let value = wrapped_value.deref();
+//!
+//! // alternatives for dereferencing:
+//! // let value = *wrapped_value;
+//! // let value: &NonSendType = &wrapped_value;
+//!
+//! // alternatives for mutable dereferencing (value and wrapped_value must be mutable too, then):
+//! // let mut value = wrapped_value.deref_mut();
+//! // let mut value = &mut *wrapped_value;
+//! // let mut value: &mut NonSendType = &mut wrapped_value;
+//! ```
+//!
+//! [`Send`]: https://doc.rust-lang.org/std/marker/trait.Send.html
+//! [`gtk-rs`]: http://gtk-rs.org/
+//! [`GTK+`]: https://www.gtk.org/
+//! [using `Glib`]: http://gtk-rs.org/docs/glib/source/fn.idle_add.html
+
 use std::ops::{Drop,Deref,DerefMut};
 use std::marker::Send;
 use std::thread;
@@ -14,6 +80,8 @@ use std::thread::ThreadId;
 const DEREF_ERROR: &'static str = "Dropped SendWrapper<T> variable from a thread different to the one it has been created with.";
 const DROP_ERROR: &'static str = "Dereferenced SendWrapper<T> variable from a thread different to the one it has been created with.";
 
+/// A wrapper which allows you to move around non-[`Send`]-types between threads, as long as you access the contained
+/// value only from within the original thread and make sure that it is dropped from within the original thread.
 pub struct SendWrapper<T> {
 	data: *mut T,
 	thread_id: ThreadId,
@@ -22,6 +90,7 @@ pub struct SendWrapper<T> {
 impl<T> SendWrapper<T> {
 
 	/// Create a SendWrapper<T> wrapper around a value of type T.
+	/// The wrapper takes ownership of the value.
 	pub fn new(data: T) -> SendWrapper<T> {
 		SendWrapper {
 			data: Box::into_raw(Box::new(data)),
@@ -29,6 +98,7 @@ impl<T> SendWrapper<T> {
 		}
 	}
 
+	/// Returns if the value can be safely accessed from within the current thread.
 	pub fn valid(&self) -> bool {
 		self.thread_id == thread::current().id()
 	}
@@ -40,41 +110,54 @@ unsafe impl<T> Send for SendWrapper<T> { }
 impl<T> Deref for SendWrapper<T> {
 	type Target = T;
 
-	/// Dereference to the contained value
+	/// Returns a reference to the contained value.
 	///
 	/// # Panics
-	/// Derefencing panics, if it is called from a different thread than the
-	/// one the SendWrapper<T> instance has been created with.
+	/// Derefencing panics if it is done from a different thread than the one the SendWrapper<T> instance has been
+	/// created with.
 	fn deref(&self) -> &T {
 		if !self.valid() {
 			panic!(DEREF_ERROR);
 		}
 		unsafe {
+			// Access the value. We just checked that it is valid.
 			&*self.data
 		}
 	}
 }
 
 impl<T> DerefMut for SendWrapper<T> {
-	/// Dereference to the contained value
+
+	/// Returns a mutable reference to the contained value.
 	///
 	/// # Panics
-	/// Derefencing panics, if it is called from a different thread than the
-	/// one the SendWrapper<T> instance has been created with.
+	/// Derefencing panics if it is done from a different thread than the one the SendWrapper<T> instance has been
+	/// created with.
 	fn deref_mut(&mut self) -> &mut T {
 		if !self.valid() {
 			panic!(DEREF_ERROR);
 		}
 		unsafe {
+			// Access the value. We just checked that it is valid.
 			&mut *self.data
 		}
 	}
 }
 
 impl<T> Drop for SendWrapper<T> {
+
+	/// Drops the contained value.
+	///
+	/// # Panics
+	/// Dropping panics if it is done from a different thread than the one the SendWrapper<T> instance has been
+	/// created with. As an exception, there is no extra panic if the thread is already panicking/unwinding. This is
+	/// because otherwise there would be double panics (usually resulting in an abort) when dereferencing from a wrong
+	/// thread.
 	fn drop(&mut self) {
 		if self.valid() {
 			unsafe {
+				// Create a boxed value from the raw pointer. We just checked that the pointer is valid.
+				// Box handles the dropping for us when _dropper goes out of scope.
 				let _dropper = Box::from_raw(self.data);
 			}
 		} else {
@@ -94,11 +177,12 @@ mod tests {
 	use std::thread;
 	use std::sync::mpsc::channel;
 	use std::ops::Deref;
+	use std::rc::Rc;
 
 	#[test]
 	fn test_deref() {
 		let (sender, receiver) = channel();
-		let w = SendWrapper::new(42);
+		let w = SendWrapper::new(Rc::new(42));
 		{
 			let _x = w.deref();
 		}
@@ -115,7 +199,7 @@ mod tests {
 
 	#[test]
 	fn test_deref_panic() {
-		let w = SendWrapper::new(42);
+		let w = SendWrapper::new(Rc::new(42));
 		let t = thread::spawn(move || {
 			let _x = w.deref();
 		});
@@ -125,7 +209,7 @@ mod tests {
 
 	#[test]
 	fn test_drop_panic() {
-		let w = SendWrapper::new(42);
+		let w = SendWrapper::new(Rc::new(42));
 		let t = thread::spawn(move || {
 			let _x = w;
 		});
@@ -135,7 +219,7 @@ mod tests {
 
 	#[test]
 	fn test_valid() {
-		let w = SendWrapper::new(5usize);
+		let w = SendWrapper::new(Rc::new(42));
 		assert!(w.valid());
 		thread::spawn(move || {
 			assert!(!w.valid());
